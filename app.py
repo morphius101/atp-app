854dc522a4436589e7cfca2a2d6f1c75e93beeda
Jeremy Lykken <jlykken@kodinggriz.awsapps.com>
2020-01-27 22:35:57 -0500

    added player class

--- app.py
+++ app.py
@@ -3,6 +3,18 @@
 # Database - Player Bios
 player_db = { }
 
+class Player:
+    def __init__(self):
+        self.name = name
+        self.age = age
+        self.start = start
+        self.country = country
+        self.height = height
+        self.plays = plays
+
+    def __repr__(self):
+        return '<Class Object: Player>'
+
 # Character User Interface - User
 print('-----------------------------------')
 print('Welcome to the 2020 ATP Player App!')
