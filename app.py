5bad41e713095af10dbd5cbec136ac3746b5e434
Jeremy Lykken <jlykken@kodinggriz.awsapps.com>
2020-01-27 22:20:11 -0500

    initial project version

---
+++ app.py
@@ -0,0 +1,16 @@
+# Import Python Libraries
+
+# Database - Player Bios
+player_db = { }
+
+# Character User Interface - User
+print('-----------------------------------')
+print('Welcome to the 2020 ATP Player App!')
+print('The menu options are as follows:')
+print('V - View a player')
+print('A - Add a player')
+print('E - Edit a player')
+print('R - Remove a player')
+print('-----------------------------------')
+
+
